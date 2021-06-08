6fe4ea1516900cdb2a383cd760df753d70992177
Kyle Feller <>
2021-06-08 00:14:47 +0000

    Finished!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function findMinValue(arr){
   let min = arr[0];
   for (i = 0; i < arr.length; i++){
     if (arr[i] < min){
-      min = arr[i];
+      min = arr[i]
     }
   }
   return min;
@@ -20,9 +20,22 @@ f) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
 
+function sortedArray(array){
+let sortedNumbers = [];
+
+    while(array.length>0){
+   sortedNumbers.push(findMinValue(array));
+   array.splice(array.indexOf(findMinValue(array)),1);
+  
+  }
+
+return sortedNumbers;
+}
+
 
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(sortedArray(nums3));
